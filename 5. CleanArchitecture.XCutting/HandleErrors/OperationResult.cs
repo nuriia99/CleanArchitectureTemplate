c229c8baa8c427613cb298381049b96da1d79116
Nuria Antón Calle <nurianton1999@gmail.com>
2026-07-01 08:33:36 +0200

    complete handleErrors class

--- 5. CleanArchitecture.XCutting/HandleErrors/OperationResult.cs
+++ 5. CleanArchitecture.XCutting/HandleErrors/OperationResult.cs
@@ -13,10 +13,17 @@
             Errors = [];
         }
 
+        public OperationResult(List<Error> errors)
+        {
+            Errors = errors;
+        }
+
         public T? Result { get; private set; }
 
-        public List<Error>? Errors { get; }
+        public List<Error>? Errors { get; private set; }
 
         public bool HasErrors() => Errors != null && Errors.Count != 0;
+        public OperationResult<T> AddErrors(List<Error> errors) => new(errors);
+        public OperationResult<T> AddResult(T result) => new(result);
     }
 }
