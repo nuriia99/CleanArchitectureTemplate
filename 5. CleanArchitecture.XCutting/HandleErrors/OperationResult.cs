b63c77930bc721aa10b90fbd286515a1169722e6
Nuria Antón Calle <nurianton1999@gmail.com>
2026-07-01 08:37:21 +0200

    add error method in OperationResult class

--- 5. CleanArchitecture.XCutting/HandleErrors/OperationResult.cs
+++ 5. CleanArchitecture.XCutting/HandleErrors/OperationResult.cs
@@ -18,12 +18,18 @@
             Errors = errors;
         }
 
+        public OperationResult(Error error)
+        {
+            Errors = [error];
+        }
+
         public T? Result { get; private set; }
 
         public List<Error>? Errors { get; private set; }
 
         public bool HasErrors() => Errors != null && Errors.Count != 0;
         public OperationResult<T> AddErrors(List<Error> errors) => new(errors);
+        public OperationResult<T> AddError(Error error) => new(error);
         public OperationResult<T> AddResult(T result) => new(result);
     }
 }
